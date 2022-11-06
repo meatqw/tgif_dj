d916cdc25f740b1c1d8c23004b0fb6d5d2b86be9
olega211 <106370448+olega211@users.noreply.github.com>
2022-11-06 16:19:03 +0700

    f

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 import sys
 
-path = "/var/www/tgif_dj/"
+path = "/var/www/tgif_dj/app"
 if path not in sys.path:
     sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
