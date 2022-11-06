5fc3357253f7f92b40f1563b37200cced039f14f
olega211 <106370448+olega211@users.noreply.github.com>
2022-11-07 03:48:22 +0700

    new pr structure

--- app/wsgi.py
+++ app/wsgi.py
@@ -10,10 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 import sys
 
-path = "/var/www/tgif_dj/app"
-if path not in sys.path:
-    sys.path.insert(0, path)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
